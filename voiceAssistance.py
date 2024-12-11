ec8799c9f4a7c23ff188ed71bcea30207015e220
jatintaliyan <jatintaliyan91@gmail.com>
2024-12-12 01:38:41 +0530

    Create voiceAssistance.py

---
+++ voiceAssistance.py
@@ -0,0 +1,104 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("good morning!")
+    elif hour>=12 and hour<18:
+        speak("good afternoon!")
+    else:
+        speak("good evening!")
+    speak("how can i help you")
+
+def takeCommand():
+    #it takes microphone input from user and return string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listning...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    
+    
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("say that again please...")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'say something' in query:
+            speak(' -----     aaah gaand dee gha keee ithnee gooor seee suunn raah')
+        
+        elif 'play music' in query:
+            music_dir = 'D:\\songs'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[1]))
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            print(strTime)
+            speak(f"the time is {strTime}")
+
+        elif 'open firefox' in query:
+            firefoxPath = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+            os.startfile(firefoxPath)
+
+        # elif 'email to harry' in query:
+        #     try:
+        #         speak("what should i say")
+        #         content = takeCommand()
+        #         to = "jatintaliyan2@gmail.com"
+        #         sendEmail(to, content)
+        #         speak("Email has been sent!")
+        #     except Exception as e:
+        #         print(e)
+        #         speak("not able to send email")
+        elif 'close' in query:
+            speak("good day sir")
+            break
+
