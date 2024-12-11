80a7d1552d5e676f85e7c589b2125a54641cdb62
jatintaliyan <jatintaliyan91@gmail.com>
2024-12-12 01:38:36 +0530

    Create tempCodeRunnerFile.py

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+        # elif 'email to harry' in query:
+        #     try:
+        #         speak("what should i say")
+        #         content = takeCommand()
+        #         to = "jatintaliyan2@gmail.com"
+        #         sendEmail(to, content)
+        #         speak("Email has been sent!")
+        #     except Exception as e:
+        #         print(e)
+        #         speak("not able to send email")
