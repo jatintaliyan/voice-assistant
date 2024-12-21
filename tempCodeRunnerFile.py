00250836e3ae7e0e368ec4d0f581df5c6b71da8d
jatintaliyan <jatintaliyan91@gmail.com>
2024-12-21 18:26:40 +0530

    Create tempCodeRunnerFile.py

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,31 @@
+   elif 'mayank' in query:
+            print('lund le le gandu')
+            speak(f"mayank {gali[random.randint(0,9)]}")
+            
+        elif 'anuvansh' in query:
+            print('lund le le gandu')
+            speak(f"Anu vansh {gali[random.randint(0,9)]}")
+
+        elif 'manu' in query:
+            print('lund le le gandu')
+            speak(f"manu {gali[random.randint(0,9)]}")
+
+        elif 'vansh' in query:
+            print('lund le le gandu')
+            speak(f"vansh {gali[random.randint(0,9)]}")
+
+        elif 'krish' in query:
+            print('lund le le gandu')
+            speak(f"krish {gali[random.randint(0,9)]}")
+
+        elif 'rajat' in query:
+            print('lund le le gandu')
+            speak(f"rajat {gali[random.randint(0,9)]}")
+
+        elif 'hardik' in query:
+            print('lund le le gandu')
+            speak(f"Hardik {gali[random.randint(0,9)]}")
+
+        elif 'bhosdi ke' in query:
+            print('aukat me')
+            speak("lodu gaalii kiisse dee raah")
